c7f22d99121071d6da5b862563fd6d18336529a8
LomadaYamini <153385050+LomadaYamini@users.noreply.github.com>
2026-04-23 18:48:27 +0530

    Update CustomerController.java

--- src/main/java/com/accenture/lkm/web/controller/CustomerController.java
+++ src/main/java/com/accenture/lkm/web/controller/CustomerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.lkm.bean.CustomerBean;
 import com.accenture.lkm.service.CustomerService;
-
+@CrossOrigin(origins = "*")
 @RestController
 
 @RequestMapping("/customer/controller")
