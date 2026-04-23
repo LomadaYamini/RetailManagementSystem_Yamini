efa7d8efca2b494217a7b6410661e187860d0f4b
LomadaYamini <153385050+LomadaYamini@users.noreply.github.com>
2026-04-23 18:49:46 +0530

    Update OrderController.java

--- src/main/java/com/accenture/lkm/web/controller/OrderController.java
+++ src/main/java/com/accenture/lkm/web/controller/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.lkm.bean.OrderBean;
 import com.accenture.lkm.service.OrderService;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/order/controller")
 public class OrderController {
